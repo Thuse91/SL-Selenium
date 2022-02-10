aec169632ce27786686730aeb53838382656d2e6
Jimmy Thuse <thuse91@live.se>
2022-02-10 17:51:55 +0100

    adding chromedriver as test

---
+++ index.js
@@ -0,0 +1,69 @@
+const {By,Key,Builder} = require("selenium-webdriver");
+require("chromedriver");
+
+
+
+var searchString = "Automation testing with Selenium";
+
+async function Swaglabs_standard_login() {
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("https://www.saucedemo.com/");
+    await driver.findElement(By.id("user-name")).click();
+    await driver.findElement(By.id("user-name")).sendKeys("standard_user");
+    await driver.findElement(By.id("password")).sendKeys("secret_sauce",Key.RETURN)
+    var title = await driver.getTitle();
+    console.log('Title is:',title,searchString);
+    console.log("First test - Swag Labs Standard Login")
+    console.log("Test Passed")
+    await driver.quit();
+}
+Swaglabs_standard_login();
+
+
+async function Swaglabs_locked_login() {
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("https://www.saucedemo.com/");
+    await driver.findElement(By.id("user-name")).click();
+    await driver.findElement(By.id("user-name")).sendKeys("locked_out_user");
+    await driver.findElement(By.id("password")).sendKeys("secret_sauce",Key.RETURN)
+    var title = await driver.getTitle();
+    console.log('Title is:',title,searchString);
+    console.log("Second test - Swag Labs Locked Login")
+    console.log("Test Passed")
+    await driver.quit();
+}
+
+setTimeout(function() { Swaglabs_locked_login(); }, 2000);
+
+
+async function Swaglabs_problem_login() {
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("https://www.saucedemo.com/");
+    await driver.findElement(By.id("user-name")).click();
+    await driver.findElement(By.id("user-name")).sendKeys("problem_user");
+    await driver.findElement(By.id("password")).sendKeys("secret_sauce",Key.RETURN)
+    var title = await driver.getTitle();
+    console.log('Title is:',title,searchString);
+    console.log("Third test - Swag Labs Problem Login")
+    console.log("Test Passed")
+    await driver.quit();
+}
+
+setTimeout(function() { Swaglabs_problem_login(); }, 4000);
+
+
+async function Swaglabs_glitch_login() {
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("https://www.saucedemo.com/");
+    await driver.findElement(By.id("user-name")).click();
+    await driver.findElement(By.id("user-name")).sendKeys("performance_glitch_user");
+    await driver.findElement(By.id("password")).sendKeys("secret_sauce",Key.RETURN)
+    var title = await driver.getTitle();
+    console.log('Title is:',title,searchString);
+    console.log("Fourth test - Swag Labs Glitched Login")
+    console.log("Test Passed")
+    await driver.quit();
+}
+
+setTimeout(function() { Swaglabs_glitch_login(); }, 6000);
+
